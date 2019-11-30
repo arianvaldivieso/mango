e5f3fcd7595938d116e03e79f769dc3156db3789
Arian Valdivieso@gmail <arianvaldivieso@gmail.com>
2019-11-30 13:08:34 -0400

    merge home in develop

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+
+const routes: Routes = [
+
+  { path: 'home',      component: HomeComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
